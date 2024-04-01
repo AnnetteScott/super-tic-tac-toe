cce654ad65a980913e05cfe825d6d49f12fc1f9f
AnnetteScott <notnatural21@gmail.com>
2024-04-01 19:34:30 +1300

    set up project

---
+++ src/components/board.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'PlanEditor',
+	props: {
+		
+	},
+	setup(){
+		return {
+			
+		}
+	},
+	data() {
+		return {
+			
+		};
+	},
+	methods: {
+	}
+})
+
+</script>
+
+<template>
+	
+</template>
+
+<style scoped>
+
+</style>
