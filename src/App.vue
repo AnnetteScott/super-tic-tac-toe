33930251dc673a9c77d8d74833e1410e90f11756
AnnetteScott <notnatural21@gmail.com>
2024-04-01 22:02:35 +1300

    Display board on page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <script setup lang="ts">
-
+import Board from '@/components/board.vue'
+import BoardManager from '@/boardManager'
+new BoardManager()
 </script>
 
 <template>
 	<main>
-		<div id="board">
-
+		<div class="board_container master_square">
+			<Board />
+			<Board />
+			<Board />
+			<Board />
+			<Board />
+			<Board />
+			<Board />
+			<Board />
+			<Board />
 		</div>
 	</main>
 </template>
 
-<style scoped>
+<style>
 @import url("../public/root.css");
+.board_container{
+	width: min-content;
+	height: min-content;
+	display: grid;
+	grid-template-columns: auto auto auto;
+	padding: 11px;
+}
+
+.master_square {
+	padding: 0;
+}
+
+.small_square{
+	width: 30px;
+	height: 30px;
+}
+
+.square:nth-child(n+4):nth-child(-n+6){
+	border-bottom: 1px solid black;
+	border-top: 1px solid black;
+}
+
+.square:nth-child(3n+2){
+	border-left: 1px solid black;
+	border-right: 1px solid black;
+}
 </style>
