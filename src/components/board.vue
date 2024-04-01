8bcd64add100098aa6eae7d2943027e89940fc18
AnnetteScott <notnatural21@gmail.com>
2024-04-01 22:02:11 +1300

    Set up board visual

--- src/components/board.vue
+++ src/components/board.vue
@@ -3,9 +3,9 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
 	name: 'PlanEditor',
-	props: {
+	/* props: {
 		
-	},
+	}, */
 	setup(){
 		return {
 			
@@ -17,13 +17,26 @@ export default defineComponent({
 		};
 	},
 	methods: {
+		action(row: number, col: number){
+			
+		}
 	}
 })
 
 </script>
 
 <template>
-	
+	<div class="board_container square">
+		<div class="small_square square" @click="action(1, 1)"></div>
+		<div class="small_square square" @click="action(1, 2)"></div>
+		<div class="small_square square" @click="action(1, 3)"></div>
+		<div class="small_square square" @click="action(2, 1)"></div>
+		<div class="small_square square" @click="action(2, 2)"></div>
+		<div class="small_square square" @click="action(2, 3)"></div>
+		<div class="small_square square" @click="action(3, 1)"></div>
+		<div class="small_square square" @click="action(3, 2)"></div>
+		<div class="small_square square" @click="action(3, 3)"></div>
+	</div>
 </template>
 
 <style scoped>
